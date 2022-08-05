64a7147b1154e5bae0e68aeca6da4382d089def2
Alex Zaslavskis <sahsariga111@gmail.com>
2022-08-05 14:41:27 +0300

    added many strange things for debug

--- webxr-typescript/src/index.ts
+++ webxr-typescript/src/index.ts
@@ -1,10 +1,30 @@
-import { ArcRotateCamera, CloudPoint, Color4, Engine, Mesh, MeshBuilder, PointLight, PointsCloudSystem, Scalar, Scene, Vector3 } from "babylonjs";
+import {
+  ArcRotateCamera,
+  CloudPoint,
+  Color4,
+  Engine,
+  HemisphericLight,
+  MeshBuilder,
+  PointLight,
+  PointsCloudSystem,
+  Scalar,
+  Scene,
+  Vector3
+} from "babylonjs";
+
+const daudzums = 500
+const objectSize = 0.1; // в метрах
+const skudraSize = 8; // в пикселях
+const atrums = 0.001; // в метрах
+const dzirde = 0.1; // в метрах
+const foodDistance = randomPolarToCartesian(0.3, 0.5)
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
 // Load the 3D engine
 var engine: Engine = null;
 const sceneToRender = null;
+
 const createDefaultEngine = function () {
   return new Engine(canvas, true, {
     preserveDrawingBuffer: true,
@@ -12,20 +32,28 @@ const createDefaultEngine = function () {
   });
 };
 
-const dzirde = 50;
 enum Vieta { Bariba, Maja }
 
 class Skudra {
   virziens = Vector3.Zero()
-  atrums = 0
+  atrums = atrums
   mekle = Vieta.Bariba
   kliegs = Vieta.Maja
   lidzMajai = 0
   lidzBaribai = 0
-  constructor(virziens: Vector3, atrums: number) {
+
+  constructor(virziens: Vector3) {
     this.virziens = virziens;
-    this.atrums = atrums;
+    this.atrums = virziens.length();
   }
+}
+
+function line(from: Vector3, to: Vector3) {
+  // const line = MeshBuilder.CreateLines("lines", {
+  //   points: [from, to],
+  //   updatable: true
+  // });
+  // setTimeout(() => line.dispose(), 20)
 }
 
 function dzird(
@@ -35,145 +63,182 @@ function dzird(
 
   if (sadzirdetaVieta == Vieta.Maja &&
     sadzirdetsAttalums < skudraKasDzird.lidzMajai) {
+
     skudraKasDzird.lidzMajai = sadzirdetsAttalums
+
     if (sadzirdetaVieta == skudraKasDzird.mekle) {
       // меняем направление на кричащую букаху
       // зная где кричащая букаха, нужно посчитать вектор в направлении кричащей букахи,
       // но динной в скорость слышащей букахи
-      skudraKasDzird.virziens = kliedzosasSkudrasVieta.subtract(skudrasKasDzirdVieta)
-      skudraKasDzird.virziens.normalizeFromLength(skudraKasDzird.atrums)
+      skudraKasDzird.virziens =
+        kliedzosasSkudrasVieta
+          .subtract(skudrasKasDzirdVieta)
+          .normalize()
+          .scaleInPlace(skudraKasDzird.atrums)
+      // console.log(`дом ${skudraKasDzird.virziens.length()}`)
+      
+      line(kliedzosasSkudrasVieta, skudrasKasDzirdVieta)
     }
   }
 
   if (sadzirdetaVieta == Vieta.Bariba &&
     sadzirdetsAttalums < skudraKasDzird.lidzBaribai) {
+
     skudraKasDzird.lidzBaribai = sadzirdetsAttalums
+
     if (sadzirdetaVieta == skudraKasDzird.mekle) {
       // меняем направление на кричащую букаху
       // зная где кричащая букаха, нужно посчитать вектор в направлении кричащей букахи,
       // но динной в скорость слышащей букахи
-      skudraKasDzird.virziens = kliedzosasSkudrasVieta.subtract(skudrasKasDzirdVieta)
-      skudraKasDzird.virziens.normalizeFromLength(skudraKasDzird.atrums)
+      skudraKasDzird.virziens =
+        kliedzosasSkudrasVieta
+          .subtract(skudrasKasDzirdVieta)
+          .normalize()
+          .scaleInPlace(skudraKasDzird.atrums)
+      // console.log(`хавка ${skudraKasDzird.virziens.length()}`)
+      line(kliedzosasSkudrasVieta, skudrasKasDzirdVieta)
     }
   }
 }
 
-function kliedz(distance: number,
+function kliedz(
   skudra: Skudra, skudrasVieta: Vector3,
+  distance: number,
   citaSkudra: Skudra, citasSkudrasVieta: Vector3) {
 
-  // выясняем что кричали в прошлый раз и кричим другое
+  // что в этот раз надо кричать?
   switch (skudra.kliegs) {
     case Vieta.Maja: {
+      skudra.kliegs = Vieta.Bariba
+
       if (distance <= dzirde) dzird(
         skudrasVieta,
         Vieta.Maja, skudra.lidzMajai + dzirde,
         citaSkudra, citasSkudrasVieta)
-      skudra.kliegs = Vieta.Bariba
     }
     case Vieta.Bariba: {
+      skudra.kliegs = Vieta.Maja
+
       if (distance <= dzirde) dzird(
         skudrasVieta,
         Vieta.Bariba, skudra.lidzBaribai + dzirde,
         citaSkudra, citasSkudrasVieta)
-      skudra.kliegs = Vieta.Maja
     }
   }
 }
 
+function polarToCartesian(radius: number, phi: number, theta: number) {
+  const x = radius * Math.sin(phi) * Math.cos(theta);
+  const y = radius * Math.sin(phi) * Math.sin(theta);
+  const z = radius * Math.cos(phi);
+  return new Vector3(x, y, z);
+}
+
+function randomPolarToCartesian(radiusMin: number, radiusMax: number) {
+  return polarToCartesian(
+    Scalar.RandomRange(radiusMin, radiusMax),
+    Scalar.RandomRange(0, Math.PI),
+    Scalar.RandomRange(0, Scalar.TwoPi))
+}
 
 const createScene = async function () {
   const scene = new Scene(engine);
 
   // Create camera and light
-  const light = new PointLight("Point", new Vector3(5, 10, 5), scene);
   const camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 2, new Vector3(0, 0, 0), scene);
   camera.attachControl(canvas, true);
 
+  // const light = new PointLight("Point", new Vector3(5, 10, 5), scene);
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
+  // создаём дом и еду
+  const maja = MeshBuilder.CreateSphere("maja", { diameter: objectSize }, scene);
 
-
-  
+  maja.position = new Vector3(0, 0.5, 0)
+  const bariba = MeshBuilder.CreateBox("box", { size: objectSize }, scene);
+  bariba.position = maja.position.add(foodDistance)
 
   //Create a manager for the player's sprite animation
-  const pcs = new PointsCloudSystem("pcs", 2, scene);
+  const pcs = new PointsCloudSystem("pcs", skudraSize, scene);
   pcs.computeBoundingBox = true;
   const skudras: Skudra[] = [];
 
   const spawn = function (particle: CloudPoint, i: number) {
-    particle.position = Vector3.Zero()
-    particle.color = new Color4(Math.random(), Math.random(), Math.random(), Math.random());
-    const r = 0.01; //Math.random()/1000;
-    const phi = Scalar.RandomRange(0, Math.PI)
-    const theta = Scalar.RandomRange(0, Scalar.TwoPi)
-    const x = r * Math.cos(phi) * Math.sin(theta)
-    const y = r * Math.sin(phi) * Math.sin(theta)
-    const z = r * Math.cos(theta)
-    const virziens = new Vector3(x, y, z)
-    skudras[i] = new Skudra(virziens, virziens.length())
+    particle.color = new Color4(Math.random(), Math.random(), Math.random(), 1);
+    const virziens = randomPolarToCartesian(atrums,atrums)
+    skudras[i] = new Skudra(virziens)
+
+    particle.position = virziens
+      .normalizeToNew()
+      .scaleInPlace(objectSize / 2)
+      .addInPlace(maja.position)
   }
 
-  pcs.addPoints(1000, spawn);
+  pcs.addPoints(daudzums, spawn);
   pcs.buildMeshAsync();
 
   pcs.updateParticle = function (particle) {
-
-
-
+    const skudra = skudras[particle.idx]
 
     // букаха походила
-    particle.position.addInPlace(skudras[particle.idx].virziens);
+    particle.position.addInPlace(skudra.virziens);
 
-    // букаха увеличила все счётчики на велечину скорости
-    const skudra = skudras[particle.idx]
+    // букаха увеличила все счётчики на велечину своей скорости
     skudra.lidzMajai += skudra.atrums
     skudra.lidzBaribai += skudra.atrums
+    // skudra.lidzMajai += 1
+    // skudra.lidzBaribai += 1
+
+    // проверить не уткнулись ли в еду или дом
+    // обнулить сообтветсвующий счётчик
+    // поменять skudra.mekle на противоположный
+    if (particle.intersectsMesh(bariba, false)) {
+      skudra.lidzBaribai = 0
+
+      if (skudra.mekle == Vieta.Bariba) {
+        // console.log('нашёл еду!')
+        skudra.mekle = Vieta.Maja
+        particle.color = new Color4(1, 0, 0, 1)
+        // разворот на 180 градусов
+        skudra.virziens.scaleInPlace(-1)
+      }
+    }
+
+    if (particle.intersectsMesh(maja, true)) {
+      skudra.lidzMajai = 0
+
+      if (skudra.mekle == Vieta.Maja) {
+        // console.log('нашёл дом!')
+        skudra.mekle = Vieta.Bariba
+        particle.color = new Color4(0, 0, 1, 1)
+        // разворот на 180 градусов
+        skudra.virziens.scaleInPlace(-1)
+      }
+    }
 
     // букаха кричит одно из пройденных путей
     // Ищем кто услышал,
-    //      чтобы два раза не прогонять по массиву сразу меняемся данными в обе стороны
-    //      и проганяем только оставшихся (такая вот оптимизация)
+    // чтобы два раза не прогонять по массиву сразу меняемся данными в обе
+    // стороны и прогоняем только оставшихся (такая вот оптимизация)
     for (var p = particle.idx + 1; p < pcs.nbParticles; p++) {
       const citaSkudra = skudras[p]
       const citasSkudrasVieta = pcs.particles[p].position
       const distance = Vector3.Distance(particle.position, citasSkudrasVieta);
-      kliedz(distance, skudra, particle.position, citaSkudra, citasSkudrasVieta)
-      kliedz(distance, citaSkudra, citasSkudrasVieta, skudra, particle.position)
-      if(Vector3.Distance(particle.position,bariba_object.position)==0){
-        console.log('мы уперлись во что-то!')
-
-      }
-      
-     
+      // if (distance <= dzirde) console.log('кто-то рядом')
+      kliedz(skudra, particle.position, distance, citaSkudra, citasSkudrasVieta)
+      kliedz(citaSkudra, citasSkudrasVieta, distance, skudra, particle.position)
     }
-
-    
-
-    // TODO: проверить не уткнулись ли в еду или дом
-    //      обнулить сообтветсвующий счётчик
-    //      поменять skudra.mekle на противоположный
 
     return particle
   }
 
-
-
-  const bariba_object = MeshBuilder.CreateBox("box", {size:0.1}, scene);
-  bariba_object.position=new Vector3(Math.random(),Math.random(),Math.random());
- 
-
   scene.registerAfterRender(() => pcs.setParticles())
-
-  const env = scene.createDefaultEnvironment();
-
-
+  // const env = scene.createDefaultEnvironment();
 
   // initialize XR
   const xr = await scene.createDefaultXRExperienceAsync({
-    floorMeshes: [env.ground]
-
+    // floorMeshes: [env.ground]
   });
- 
 
   return scene;
 }
