72247a5d709a2c2af4a932eea9ec555acf41d49a
Ed Ilyin <Ed.Il@live.com>
2022-08-11 21:03:21 +0300

    simplify main

--- swarm/src/main.ts
+++ swarm/src/main.ts
@@ -1,33 +1,25 @@
 'use strict';
 import './style.css'
-import "@babylonjs/loaders/glTF";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { World } from "./world";
+// Required for EnvironmentHelper
+import '@babylonjs/core/Materials/Textures/Loaders'
+// Enable GLTF/GLB loader for loading controller models from WebXR Input registry
+import '@babylonjs/loaders/glTF'
+// Without this next import, an error message like this occurs loading controller models:
+//  Build of NodeMaterial failed" error when loading controller model
+//  Uncaught (in promise) Build of NodeMaterial failed: input rgba from block
+//  FragmentOutput[FragmentOutputBlock] is not connected and is not optional.
+import '@babylonjs/core/Materials/Node/Blocks'
 
 const canvas = document.getElementById("app") as HTMLCanvasElement;
 
-const createDefaultEngine = () =>
-  new Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-
 // Load the 3D engine
-var engine: Engine
-
-// Create a default engine to load the scene
-try { engine = createDefaultEngine(); } catch (e) {
-  console.log(
-    "the available createEngine function failed. Creating the default engine instead"
-  );
-
-  engine = createDefaultEngine();
-}
-
-if (!engine) throw "engine should not be null.";
+const engine = new Engine(canvas, true);
 
 //Create the scene
-new World().createScene(engine, canvas).then(sceneToRender => {
-  engine.runRenderLoop(() => sceneToRender.render());
-});
+const scene = await new World().createScene(engine, canvas)
+engine.runRenderLoop(() => scene.render());
 
 // Resize the engine to fit the scene
 window.addEventListener("resize", () => engine.resize());
-
