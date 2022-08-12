c7081e22b3d88f4e8ee24a7df08322c4769b45ad
Alex Zaslavskis <sahsariga111@gmail.com>
2022-08-12 14:59:21 +0300

    size matters

--- swarm/src/nearmenu.ts
+++ swarm/src/nearmenu.ts
@@ -6,6 +6,9 @@ import { Scene } from "@babylonjs/core/scene";
 import { TouchHolographicButton } from "@babylonjs/gui/3D/controls/touchHolographicButton";
 import { WebXRDefaultExperience } from "@babylonjs/core/XR/webXRDefaultExperience";
 import { WebXRFeatureName } from "@babylonjs/core/XR/webXRFeaturesManager";
+import {Slider3D, StackPanel3D} from "@babylonjs/gui";
+import { Vector3 } from "@babylonjs/core";
+
 
 export function create_menu(scene: Scene, xr: WebXRDefaultExperience, colony: Colony) {
 
@@ -15,8 +18,11 @@ export function create_menu(scene: Scene, xr: WebXRDefaultExperience, colony: Co
         console.log("Articulated hand tracking not supported in this browser.");
     }
 
-    const manager = new GUI3DManager(scene);
-    manager.useRealisticScaling = true;
+    var manager = new GUI3DManager(scene);
+
+
+    manager.useRealisticScaling=true;
+
     const menu = new NearMenu("NearMenu");
     menu.rows = 3;
     manager.addControl(menu);
@@ -43,4 +49,48 @@ export function create_menu(scene: Scene, xr: WebXRDefaultExperience, colony: Co
     });
     debug.text = "Debug";
     menu.addButton(debug);
-}
+
+
+    // Create a horizontal stack panel
+    var panel = new StackPanel3D();
+    panel.margin = 0.05;
+    panel.isVertical = false;
+    panel.position.x=10;
+    panel.position.y=10;
+    panel.position.z=10;
+  
+    manager.addControl(panel);
+    panel.position.z = -1.5;
+
+    
+   
+
+
+    var slider4 = new Slider3D("slider4", true);
+    panel.addControl(slider4);
+    slider4.maximum =  2 * Math.PI;
+    slider4.scaling.x = 20;
+    slider4.scaling.y = 20;
+    slider4.scaling.z = 20;
+
+    slider4.position.x = 15;
+    slider4.position.y = 50;
+    //slider4.node.addRotation(0, 0, -Math.PI / 2);
+    slider4.onValueChangedObservable.add(function () {
+        //sphere.rotation.x = value;
+        //sphere.rotation.y = value;
+    });
+
+    scene.onAfterRenderObservable.add(() => {
+        if (slider4.mesh && !slider4.mesh.behaviors[0].dragging) {
+            slider4.value = (slider4.value + 0.01) % slider4.maximum;
+
+            // workaround for position visual update bug, copied straight from slider3D.ts:
+            const position = ((slider4.value - slider4.minimum) / (slider4.maximum - slider4.minimum)) * (slider4.end - slider4.start) + slider4.start;
+            slider4.mesh.position.x = Math.min(Math.max(position, slider4.start), slider4.end);
+        }
+    });
+
+    return scene;
+};
+
