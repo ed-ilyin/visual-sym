5d745d81d9a247c5fd1f4d8dbfb9e0af08f23c9a
Ed Ilyin <Ed.Il@live.com>
2022-08-13 23:08:11 +0300

    adds PWA

--- swarm/vite.config.ts
+++ swarm/vite.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  server: { https: true },
-  plugins: [ mkcert() ]
-})
+export default
+    defineConfig({ server: { https: true }, plugins: [mkcert(), VitePWA()] })
